f969904abbd1dcabc60816f4371f7ebd49f99505
Konstantin Shaposhnikov <shaposhnikov.k@brinex.ru>
2021-07-08 11:22:42 +0300

    Создал регистрацию и аутентификацию пользователя, переход зарегистрировнного пользователя на личную страницу, выход пользователя

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/', 'IndexController')->name('index');
+
+Route::group([
+    'as' => 'user.', // имя маршрута, например user.index
+    'prefix' => 'user', // префикс маршрута, например user/index
+    'namespace' => 'User', // пространство имен контроллеров
+    'middleware' => ['auth'] // один или несколько посредников
+], function () {
+    // главная страница
+    Route::get('index', 'IndexController')->name('index');
+});
+
+Route::group([
+    'as' => 'auth.', // имя маршрута, например auth.index
+    'prefix' => 'auth', // префикс маршрута, например auth/index
+], function () {
+    // форма регистрации
+    Route::get('register', 'Auth\RegisterController@register')
+        ->name('register');
+    // создание пользователя
+    Route::post('register', 'Auth\RegisterController@create')
+        ->name('create');
+    // форма входа
+    Route::get('login', 'Auth\LoginController@login')
+        ->name('login');
+    // аутентификация
+    Route::post('login', 'Auth\LoginController@authenticate')
+        ->name('auth');
+    // выход
+    Route::get('logout', 'Auth\LoginController@logout')
+        ->name('logout');
 });
